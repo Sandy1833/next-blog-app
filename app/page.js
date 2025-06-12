9c69309baafe82bef579ecf863a58dc3628ac614
Sandesh Naik <naiksandesh149@gmail.com>
2025-06-12 18:02:04 +0530

    env file add

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import Image from "next/image";
 import { ToastContainer } from 'react-toastify';
-
+import 'dotenv/config'
 
 export default function Home() {
   return (
